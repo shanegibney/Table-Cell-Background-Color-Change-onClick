b56bada2becf45cb2ee15ef840d82f54aa513018
shanegibney <shanegibney@gmail.com>
2017-05-26 23:02:08 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// var table = document.getElementById("myTable");
+var table = document.getElementsByTagName('table')[0];
+var cell = table.getElementsByTagName('td');
+var col, wh, lG;
+table.addEventListener('click', function(e) {
+  col = window.getComputedStyle(e.target, null).backgroundColor;
+  if (e.target.tagName === "TD") {
+    // col = e.target.tagName.backgroundColor;
+    console.log(col);
+    wh = 'rgba(0, 0, 0, 0)';
+    lG = 'rgb(144, 238, 144)';
+    if (col === wh) {
+      e.target.style.backgroundColor = lG;
+    } else {
+      e.target.style.backgroundColor = wh;
+    }
+  }
+}, false);
